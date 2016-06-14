f0661d6f0e0efa8f9646ce9dbd5274a68c99911d
Sahil Muthoo <sahil.muthoo@gmail.com>
2016-06-14 20:51:20 +0530

    Add an end to end test

--- src/main/java/com/sahilm/Application.java
+++ src/main/java/com/sahilm/Application.java
@@ -6,13 +6,25 @@ import org.springframework.core.io.ClassPathResource;
 
 public class Application {
 
+    private static Server server;
+
     public static void main(final String[] args) throws Exception {
-        final Server server = new Server(8080);
+        start(true);
+    }
+
+    public static void start(final boolean join) throws Exception {
+        server = new Server(8080);
         final WebAppContext webapp = new WebAppContext();
         webapp.setContextPath("/");
         webapp.setWar(new ClassPathResource("webapp").getURI().toString());
         server.setHandler(webapp);
         server.start();
-        server.join();
+        if (join) {
+            server.join();
+        }
+    }
+
+    public static void stop() throws Exception {
+        server.stop();
     }
 }
---
+++ src/test/java/com/sahilm/endtoend/EndToEndTest.java
@@ -0,0 +1,41 @@
+package com.sahilm.endtoend;
+
+import com.jayway.jsonpath.JsonPath;
+import com.sahilm.Application;
+import org.apache.http.client.fluent.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class EndToEndTest {
+    private static final String APP_URL = "http://localhost:8080/";
+    private static final Logger LOGGER = LoggerFactory.getLogger(EndToEndTest.class);
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        Application.start(false);
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception {
+        Application.stop();
+    }
+
+    @Test
+    public void shouldReturnTweetsByHashtag() throws Exception {
+        Request getTweets = Request.Get(APP_URL + "/tweets?hashtag=docker");
+        String response = getTweets.execute().returnContent().asString(StandardCharsets.UTF_8);
+        LOGGER.info(response);
+        List<String> tweets = JsonPath.read(response, "$..text");
+        assertThat(tweets.size()).isGreaterThan(0);
+    }
+
+
+}
