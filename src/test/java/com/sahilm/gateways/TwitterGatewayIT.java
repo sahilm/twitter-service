8537f73bab1a50fb23dbf1a1c4e1f0b5c9fad3cf
Sahil Muthoo <sahil.muthoo@gmail.com>
2016-06-14 07:27:44 +0530

    Add a boundary integration test

---
+++ src/test/java/com/sahilm/gateways/TwitterGatewayIT.java
@@ -0,0 +1,24 @@
+package com.sahilm.gateways;
+
+import com.sahilm.resources.Tweet;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ContextConfiguration(locations = "classpath:context.xml")
+@Test
+public class TwitterGatewayIT extends AbstractTestNGSpringContextTests {
+
+    @Inject
+    TwitterGateway twitterGateway;
+
+    public void shouldBeAbleToQueryTweetsByHashtag() throws Exception {
+        List<Tweet> tweets = twitterGateway.queryByHashtag("#docker");
+        assertThat(tweets).isNotEmpty();
+    }
+}
