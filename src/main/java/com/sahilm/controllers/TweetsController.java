a88dd7b35a75be4c265d9f8b506a424a92547ff1
Sahil Muthoo <sahil_muthoo@symantec.com>
2016-06-29 16:59:01 +0530

    Return runtime in seconds

--- src/main/java/com/sahilm/controllers/TweetsController.java
+++ src/main/java/com/sahilm/controllers/TweetsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(
@@ -29,7 +30,7 @@ public class TweetsController {
         long startTime = System.nanoTime();
         final List<Tweet> tweets = tweetsService.getTweetsByHashtag(hashtag);
         long estimatedTime = System.nanoTime() - startTime;
-        response.setHeader("x-runtime", String.valueOf(estimatedTime));
+        response.setHeader("x-runtime", String.valueOf(TimeUnit.SECONDS.convert(estimatedTime, TimeUnit.NANOSECONDS)));
         return tweets;
     }
 }
